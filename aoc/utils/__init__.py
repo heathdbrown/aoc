32ce2b76eb8c87fe28c7c8e097005a03a54c1f1a
Heath Brown <heathd.brown@gmail.com>
2023-05-24 11:56:02 -0500

    add common methods

--- aoc/utils/__init__.py
+++ aoc/utils/__init__.py
@@ -1,3 +1,6 @@
+import collections
+from itertools import islice
+
 def read_input_raw(file: str) -> str:
     with open(file, "r") as f:
         data = f.read()
@@ -8,3 +11,13 @@ def read_input_splitlines(file: str) -> list[str]:
     with open(file, "r") as f:
         data = f.read().splitlines()
     return data
+
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
