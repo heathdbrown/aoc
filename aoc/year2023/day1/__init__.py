970814398e768a4a88fcb0a6d4da5aac40886f91
Heath Brown <heathd.brown@gmail.com>
2023-12-12 16:38:21 -0600

    completes day1

--- aoc/year2023/day1/__init__.py
+++ aoc/year2023/day1/__init__.py
@@ -29,32 +29,49 @@ suffix = {"teen": 10}
 
 
 def parse_number(text: str):
-    digits = []
-    for char in text:
-        if char.isdigit():
-            digits.append(char)
-    if len(digits) == 2:
-        return int("".join(digits))
-    if len(digits) > 2:
-        num: str = "".join(digits)
-        first = num[0]
-        last = num[-1:]
-        return int(first + last)
-    if len(digits) == 1:
-        return int("".join(digits) * 2)
-    if len(digits) == 0:
-        return 0
+    # digits = []
+    # for char in text:
+    #     if char.isdigit():
+    #         digits.append(char)
+    # if len(digits) == 2:
+    #     return int("".join(digits))
+    # if len(digits) > 2:
+    #     num: str = "".join(digits)
+    #     first = num[0]
+    #     last = num[-1:]
+    #     return int(first + last)
+    # if len(digits) == 1:
+    #     return int("".join(digits) * 2)
+    # if len(digits) == 0:
+    #     return 0
+    p1 = 0
+    p2 = 0
+    p1_digits = []
+    p2_digits = []
+    for i, c in enumerate(text):
+        if c.isdigit():
+            p1_digits.append(c)
+            p2_digits.append(c)
+        for d, val in enumerate(
+            ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        ):
+            if text[i:].startswith(val):
+                p2_digits.append(str(d + 1))
+        # p1 += int(p1_digits[0] + p1_digits[-1])
+        p2 += int(p2_digits[0] + p2_digits[-1])
+    return p1, p2
 
 
-def parse_file(text: str) -> list[int]:
+def parse_file(text: str) -> list[tuple[int, int]]:
     return [parse_number(line) for line in text.splitlines()]
 
 
 def find_prefix(text: str):
-    return re.findall(
-        "one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen|eighteen|nineteen",
-        text,
-    )
+    # return re.findall(
+    #     "one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen|eighteen|nineteen",
+    #     text,
+    # )
+    ...
 
 
 def str_to_num(text: str):
@@ -80,5 +97,30 @@ def part1(file_path: str):
     return solve(file_path)
 
 
+def part2(file_path: str):
+    with open(file_path) as f:
+        text = f.read().strip()
+    lines = text.splitlines()
+    # https://github.com/jonathanpaulson/AdventOfCode/blob/master/2023/1.py
+    p1 = 0
+    p2 = 0
+    for line in lines:
+        p1_digits = []
+        p2_digits = []
+        for i, c in enumerate(line):
+            if c.isdigit():
+                p1_digits.append(c)
+                p2_digits.append(c)
+            for d, val in enumerate(
+                ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+            ):
+                if line[i:].startswith(val):
+                    p2_digits.append(str(d + 1))
+        # p1 += int(p1_digits[0] + p1_digits[-1])
+        p2 += int(p2_digits[0] + p2_digits[-1])
+    return p2
+
+
 if __name__ == "__main__":
-    print(part1("./data/input_2023_day1.txt"))
+    # print(part1("./data/input_2023_day1.txt"))
+    print(part2("./data/input_2023_day1.txt"))
