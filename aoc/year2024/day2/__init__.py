0a7380b6807d715665187235b527803ff6cb6afe
Heath Brown <heathd.brown@gmail.com>
2024-12-03 07:17:11 -0600

    2024 day2 hextree part1 refactor to func

--- aoc/year2024/day2/__init__.py
+++ aoc/year2024/day2/__init__.py
@@ -3,12 +3,14 @@ with open("./data/input_2024_day2.txt") as f:
     for line in f.readlines():
         reports.append([int(z) for z in line.split()])
 
-safe_count = 0
 
-for report in reports:
-    n = len(report)
-    safe_count += (all(1 <= report[i + 1] - report[i] <= 3 for i in range(n - 1))) or (
-        all(1 <= report[i] - report[i + 1] <= 3 for i in range(n - 1))
+def is_safe(l):
+    n = len(l)
+    return (all(1 <= l[i + 1] - l[i] <= 3 for i in range(n - 1))) or (
+        all(1 <= l[i] - l[i + 1] <= 3 for i in range(n - 1))
     )
+
+
+safe_count = sum(map(is_safe, reports))
 
 print(safe_count)
